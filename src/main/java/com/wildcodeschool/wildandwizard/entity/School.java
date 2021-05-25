3ef4dbac4ddb62c2167b7ba10e618eb540666ea3
cartonni <nicolas.carton@natixis.com>
2021-05-25 12:16:57 +0200

    quest done

--- src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
@@ -1,9 +1,12 @@
 package com.wildcodeschool.wildandwizard.entity;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class School {
@@ -14,7 +17,9 @@ public class School {
     private String name;
     private Long capacity;
     private String country;
-
+    @OneToMany(mappedBy="school")
+    private List<Wizard> wizards;
+    
     public School() {
     }
 
@@ -49,4 +54,13 @@ public class School {
     public void setCountry(String country) {
         this.country = country;
     }
+    
+    public void setWizards(List<Wizard> wizards) {
+        this.wizards = wizards;
+    }
+    
+    public List<Wizard> getWizards() {
+        return wizards;
+    }
+
 }
--- src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import java.sql.Date;
 
 @Entity
@@ -18,7 +21,10 @@ public class Wizard {
     private String birthPlace;
     private String biography;
     private boolean muggle;
-
+    @ManyToOne
+    @JoinColumn(name="school_id")
+    private School school;
+    
     public Wizard() {
     }
 
@@ -77,4 +83,14 @@ public class Wizard {
     public void setMuggle(boolean muggle) {
         this.muggle = muggle;
     }
+    
+    public void setSchool(School school) {
+        this.school = school;
+    }
+
+    public School getSchool() {
+        return school;
+    }
+    
+    
 }
